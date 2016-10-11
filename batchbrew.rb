031b69c941f1c9058c55c26e6b6a4c38de0ef985
MCJack123 <jackbruienne@yahoo.com>
2016-10-11 09:10:44 -0400

    Update batchbrew.rb

--- batchbrew.rb
+++ batchbrew.rb
@@ -7,7 +7,7 @@ class Batchbrew < Formula
   homepage "http://cppconsole.bruienne.com/"
   url "http://cppconsole.bruienne.com/repos/homebrew/formulae/batchbrew.1.0.tar.gz"
   version "1.0"
-  sha256 "588ef88d5c4769844063e2813a907150d2e6ea811fb393e05a474b79625d6dfe"
+  sha256 "9d38093a947111de2c151b4abe7ffb9f183c3a31505a54c5f64bd2b5bae69332"
   
   def install
     system "./install"
