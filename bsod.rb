74d08c25faf420254aaf34d548b27156810983aa
MCJack123 <jackbruienne@yahoo.com>
2016-10-11 23:19:20 -0400

    Update bsod.rb

--- bsod.rb
+++ bsod.rb
@@ -9,23 +9,13 @@ class Bsod < Formula
   version "0.1"
   sha256 "ad6eb83637d1eb655ca7891933916266bd0624e08823490083b960ff15531c43"
 
-  # depends_on "cmake" => :build
-
   def install
     system "make"
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
+    ohai "Homebrew will say that the installation is empty, but the program does work."
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test bsod`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
     system "/usr/local/bin/bsod"
   end
 end
